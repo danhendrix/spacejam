fd767147e68d8b440f9a797880b94c384f8bb7ad
thatblindgeye <thatblindgeye@gmail.com>
2021-07-16 10:04:45 -0400

    Create Questions class with a Math instance

---
+++ src/Questions/math.js
@@ -0,0 +1,13 @@
+import Questions from './questions';
+
+const mathQuestions = [
+    {
+        question: 'What is 2 + 2?',
+        options: [3, 8, 4],
+        answer: 4,
+    },
+];
+
+const Math = new Questions(mathQuestions);
+
+export default Math;
---
+++ src/Questions/questions.js
@@ -0,0 +1,27 @@
+class Questions {
+    constructor(unusedQuestions, usedQuestions = []) {
+        this.unusedQuestions = unusedQuestions;
+        this.usedQuestions = usedQuestions;
+    }
+
+    changeStatus(questionIndex) {
+        const unusedCopy = this.unusedQuestions;
+        const pickedQuestion = unusedCopy.splice(unusedCopy[questionIndex], 1);
+        this.usedQuestions.push(pickedQuestion);
+        this.unusedQuestions = unusedCopy;
+    }
+
+    pickQuestion() {
+        const randomNumber = Math.floor(
+            Math.random() * this.unusedQuestions.length
+        );
+
+        const chosenQuestion = this.unusedQuestions[randomNumber];
+        this.changeStatus(randomNumber);
+
+        // console.log(chosenQuestion);
+        return chosenQuestion;
+    }
+}
+
+export default Questions;
