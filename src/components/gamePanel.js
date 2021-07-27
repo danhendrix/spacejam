495e61d65869f6b47284fea5608e5c5070472a78
thatblindgeye <thatblindgeye@gmail.com>
2021-07-27 14:04:15 -0400

    Update styles for main menu and game panel

--- src/components/gamePanel.js
+++ src/components/gamePanel.js
@@ -3,39 +3,41 @@ import style from './style.scss';
 function GamePanel({}) {
     return (
         <>
-            <div class={`${style.keysContainer} ${style.arrowKeysContainer}`}>
-                <button
-                    id='upArrow'
-                    class={`${style.virtualKey} ${style.directionKey} ${style.upArrow}`}
-                >
-                    Up
-                </button>
-                <button
-                    id='leftArrow'
-                    class={`${style.virtualKey} ${style.directionKey} ${style.leftArrow}`}
-                >
-                    Left
-                </button>
-                <button
-                    id='downArrow'
-                    class={`${style.virtualKey} ${style.directionKey} ${style.downArrow}`}
-                >
-                    Down
-                </button>
-                <button
-                    id='rightArrow'
-                    class={`${style.virtualKey} ${style.directionKey} ${style.rightArrow}`}
-                >
-                    Right
-                </button>
-            </div>
-            <div class={`${style.keysContainer} ${style.enterKeyContainer}`}>
-                <button
-                    id='enterKey'
-                    class={`${style.virtualKey} ${style.enterKey}`}
-                >
-                    Enter
-                </button>
+            <div class={style.keysContainer}>
+                <div class={style.arrowKeysContainer}>
+                    <button
+                        id='upArrow'
+                        class={`${style.virtualKey} ${style.directionKey} ${style.upArrow}`}
+                    >
+                        Up
+                    </button>
+                    <button
+                        id='leftArrow'
+                        class={`${style.virtualKey} ${style.directionKey} ${style.leftArrow}`}
+                    >
+                        Left
+                    </button>
+                    <button
+                        id='downArrow'
+                        class={`${style.virtualKey} ${style.directionKey} ${style.downArrow}`}
+                    >
+                        Down
+                    </button>
+                    <button
+                        id='rightArrow'
+                        class={`${style.virtualKey} ${style.directionKey} ${style.rightArrow}`}
+                    >
+                        Right
+                    </button>
+                </div>
+                <div class={style.enterKeyContainer}>
+                    <button
+                        id='enterKey'
+                        class={`${style.virtualKey} ${style.enterKey}`}
+                    >
+                        Enter
+                    </button>
+                </div>
             </div>
         </>
     );
