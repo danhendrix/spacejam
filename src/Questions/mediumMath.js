4f29d5f70dc6c25722d35969be25c6599a9054c1
thatblindgeye <thatblindgeye@gmail.com>
2021-08-05 15:33:57 -0400

    Add math questions based on difficulty

---
+++ src/Questions/mediumMath.js
@@ -0,0 +1,258 @@
+import Questions from './questions';
+
+const mediumMath = new Questions([
+    // Add two 3 digit numbers
+    {
+        question: 'What is 461 + 806?',
+        answer: 1267,
+    },
+    {
+        question: 'What is 781 + 176?',
+        answer: 957,
+    },
+    {
+        question: 'What is 851 + 540?',
+        answer: 1391,
+    },
+    {
+        question: 'What is 313 + 296?',
+        answer: 609,
+    },
+    {
+        question: 'What is 111 + 334?',
+        answer: 445,
+    },
+    // Subtract a 3-digit number from any whole thousand
+    {
+        question: 'Fill in the blank: 5,000 – _ = 4,306.',
+        answer: 694,
+    },
+    {
+        question: 'Fill in the blank: 9,000 – _ = 8,873.',
+        answer: 127,
+    },
+    {
+        question: 'Fill in the blank: 6,000 – _ = 5,554.',
+        answer: 446,
+    },
+    {
+        question: 'Fill in the blank: 3,000 – _ = 2,379.',
+        answer: 621,
+    },
+    {
+        question: 'Fill in the blank: 8,000 – _ = 7,662.',
+        answer: 338,
+    },
+    // Multiple of 7
+    {
+        question: 'What is 1 x 7?',
+        answer: 7,
+    },
+    {
+        question: 'What is 2 x 7?',
+        answer: 14,
+    },
+    {
+        question: 'What is 3 x 7?',
+        answer: 21,
+    },
+    {
+        question: 'What is 4 x 7?',
+        answer: 28,
+    },
+    {
+        question: 'What is 5 x 7?',
+        answer: 35,
+    },
+    {
+        question: 'What is 6 x 7?',
+        answer: 42,
+    },
+    {
+        question: 'What is 7 x 7?',
+        answer: 49,
+    },
+    {
+        question: 'What is 8 x 7?',
+        answer: 56,
+    },
+    {
+        question: 'What is 9 x 7?',
+        answer: 63,
+    },
+    {
+        question: 'What is 10 x 7?',
+        answer: 70,
+    },
+    {
+        question: 'What is 11 x 7?',
+        answer: 77,
+    },
+    {
+        question: 'What is 12 x 7?',
+        answer: 84,
+    },
+    // Multiple of 8
+    {
+        question: 'What is 1 x 8?',
+        answer: 8,
+    },
+    {
+        question: 'What is 2 x 8?',
+        answer: 16,
+    },
+    {
+        question: 'What is 3 x 8?',
+        answer: 24,
+    },
+    {
+        question: 'What is 4 x 8?',
+        answer: 32,
+    },
+    {
+        question: 'What is 5 x 8?',
+        answer: 40,
+    },
+    {
+        question: 'What is 6 x 8?',
+        answer: 48,
+    },
+    {
+        question: 'What is 7 x 8?',
+        answer: 56,
+    },
+    {
+        question: 'What is 8 x 8?',
+        answer: 64,
+    },
+    {
+        question: 'What is 9 x 8?',
+        answer: 72,
+    },
+    {
+        question: 'What is 10 x 8?',
+        answer: 80,
+    },
+    {
+        question: 'What is 11 x 8?',
+        answer: 88,
+    },
+    {
+        question: 'What is 12 x 8?',
+        answer: 96,
+    },
+    // Multiple of 9
+    {
+        question: 'What is 1 x 9?',
+        answer: 9,
+    },
+    {
+        question: 'What is 2 x 9?',
+        answer: 18,
+    },
+    {
+        question: 'What is 3 x 9?',
+        answer: 27,
+    },
+    {
+        question: 'What is 4 x 9?',
+        answer: 36,
+    },
+    {
+        question: 'What is 5 x 9?',
+        answer: 45,
+    },
+    {
+        question: 'What is 6 x 9?',
+        answer: 54,
+    },
+    {
+        question: 'What is 7 x 9?',
+        answer: 63,
+    },
+    {
+        question: 'What is 8 x 9?',
+        answer: 72,
+    },
+    {
+        question: 'What is 9 x 9?',
+        answer: 81,
+    },
+    {
+        question: 'What is 10 x 9?',
+        answer: 90,
+    },
+    {
+        question: 'What is 11 x 9?',
+        answer: 99,
+    },
+    {
+        question: 'What is 12 x 9?',
+        answer: 108,
+    },
+    // Divisible by 10
+    {
+        question: 'What is 240 / 10?',
+        answer: 24,
+    },
+    {
+        question: 'What is 30 / 10?',
+        answer: 3,
+    },
+    {
+        question: 'What is 880 / 10?',
+        answer: 88,
+    },
+    {
+        question: 'What is 60 / 10?',
+        answer: 6,
+    },
+    {
+        question: 'What is 510 / 10?',
+        answer: 51,
+    },
+    // Divisible by whole tens
+    {
+        question: 'What is 720 / 90?',
+        answer: 8,
+    },
+    {
+        question: 'What is 630 / 70?',
+        answer: 9,
+    },
+    {
+        question: 'What is 120 / 60?',
+        answer: 2,
+    },
+    {
+        question: 'What is 480 / 80?',
+        answer: 6,
+    },
+    {
+        question: 'What is 600 / 60?',
+        answer: 10,
+    },
+    // Order of operations (five digits with two parenthesis - add, subtract)
+    {
+        question: 'What is (19 – 9) + (17 – 10 – 6)?',
+        answer: 11,
+    },
+    {
+        question: 'What is 20 + (17 – 2) – (10 + 24)?',
+        answer: 1,
+    },
+    {
+        question: 'What is (33 – 8) + (38 + 2) – 1?',
+        answer: 64,
+    },
+    {
+        question: 'What is (15 + 28) – 17 + (19 – 13) ?',
+        answer: 32,
+    },
+    {
+        question: 'What is 28 – (8 + 7) – 6 – 5?',
+        answer: 2,
+    },
+]);
+
+export default mediumMath;
