c76be85cbc2877722e80ed1792b21f56bfa3f99f
Danny Hendrix <danhendrix@gmail.com>
2021-08-07 17:16:07 -0400

    Add library game

--- src/Grids/libraryOpen.js
+++ src/Grids/libraryOpen.js
@@ -1,204 +1,6 @@
-// import Thief from '../NPC/thief';
 import Librarian from '../NPC/librarian';
-import NPC, { RequirementTypes } from '../NPC/npc';
-import mediumMath from '../Questions/mediumMath';
-import easyMath from '../Questions/easyMath';
-import hardMath from '../Questions/hardMath';
 
-const bookcase = new NPC(
-    'bookcase',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.question,
-            question: 'Write down the code to continue',
-            answer: 100,
-            afterAction: {
-                fn() {
-                    this.addToInventory('book', 1);
-                },
-                functionType: 'player',
-                message:
-                    'A hidden book slides out from behind the bookcase and reveals itself to you.',
-            },
-        },
-    ],
-);
-
-const table1 = new NPC(
-    'table1',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.question,
-            question: 'Write down the code to continue',
-            answer: 1005,
-        },
-        ...mediumMath.generateQuestions(2),
-        {
-            type: RequirementTypes.dialog,
-            message: 'Go to the table equals the square root of 25 and enter the code: 16',
-        },
-        {
-            type: RequirementTypes.inventory,
-            item: 'report card',
-            amount: 3,
-            afterAction: {
-                fn() {},
-                message: `I didn't think you could do it!`,
-            },
-            message: 'Go to the table that is the lowest prime number and enter the code: 1005',
-        },
-    ]
-);
-
-const table2 = new NPC(
-    'table3',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.question,
-            question: 'Write down the code to continue',
-            answer: 99,
-        },
-        ...hardMath.generateQuestions(1),
-        {
-            type: RequirementTypes.dialog,
-            message: 'The bookcases at the top of the room are numbered 1 to 10. Go to the bookcase that equals the square root of 100 and enter the code: 100',
-        },
-        {
-            type: RequirementTypes.inventory,
-            item: 'report card',
-            amount: 3,
-            afterAction: {
-                fn() {},
-                message: `I didn't think you could do it!`,
-            },
-            message: 'Go to the table that is the lowest prime number and enter the code: 1005',
-        },
-    ]
-);
-
-const table3 = new NPC(
-    'table3',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.question,
-            question: 'Write down the code to continue',
-            answer: 301,
-        },
-        ...hardMath.generateQuestions(3),
-        {
-            type: RequirementTypes.dialog,
-            message: 'Go to only table you have not completed and enter the code: 99',
-        },
-        {
-            type: RequirementTypes.inventory,
-            item: 'report card',
-            amount: 3,
-            afterAction: {
-                fn() {},
-                message: `I didn't think you could do it!`,
-            },
-            message: 'Go to the table that is the lowest prime number and enter the code: 1005',
-        },
-    ]
-);
-
-const table4 = new NPC(
-    'table4',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.question,
-            question: 'Write down the code to continue',
-            answer: 91,
-        },
-        ...easyMath.generateQuestions(2),
-        {
-            type: RequirementTypes.dialog,
-            message: 'Go to the table that is the lowest prime number and enter the code: 1005',
-        },
-        {
-            type: RequirementTypes.inventory,
-            item: 'report card',
-            amount: 3,
-            afterAction: {
-                fn() {},
-                message: `I didn't think you could do it!`,
-            },
-            message: 'Go to the table that is the lowest prime number and enter the code: 1005',
-        },
-    ]
-);
-
-const table5 = new NPC(
-    'table1',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.question,
-            question: 'Write down the code to continue',
-            answer: 16,
-        },
-        ...mediumMath.generateQuestions(4),
-        {
-            type: RequirementTypes.dialog,
-            message: 'Go to the table that equals one third of one half of 18 and enter the code: 301',
-        },
-        {
-            type: RequirementTypes.inventory,
-            item: 'report card',
-            amount: 3,
-            afterAction: {
-                fn() {},
-                message: `I didn't think you could do it!`,
-            },
-            message: 'Go to the table that is the lowest prime number and enter the code: 1005',
-        },
-    ]
-);
-
-const table6 = new NPC(
-    'table6',
-    null,
-    null,
-    [
-        {
-            type: RequirementTypes.dialog,
-            message: 'Your precious book is hidden somewhere in this room. It will take quite a smarty go find it.',
-        },
-        {
-            type: RequirementTypes.dialog,
-            message: 'The desks are numbered from 1 to 6 going from the left to the right. The book cases at the far end of room against the wall are numbered 1 to 10.',
-        },
-        {
-            type: RequirementTypes.dialog,
-            message: 'The book is hidden behind one of the shelves. But which one? Start by going to the desk whose number equals the square root of 16 and enter this code in the notebook sitting on the desk: 91',
-        },
-        {
-            type: RequirementTypes.inventory,
-            item: 'report card',
-            amount: 3,
-            afterAction: {
-                fn() {},
-                message: `I didn't think you could do it!`,
-            },
-            message: 'Start by going to the desk whose number equals the square root of 16 and enter this code in the notebook sitting on the desk: 91',
-        },
-    ]
-);
-
-
-
-const Library = [
+const LibraryOpen = [
     // ROW INDEX 0
     [
         {
@@ -446,7 +248,7 @@ const Library = [
         },
         {
             type: 'libraryFloorBookshelfOuter',
-            npc: bookcase,
+            npc: null,
             isAccessible: true,
             pathTo: null,
         },
@@ -713,8 +515,8 @@ const Library = [
         },
         {
             type: 'libraryFloorTable',
-            npc: table1,
-            isAccessible: true,
+            npc: null,
+            isAccessible: false,
             pathTo: null,
         },
         {
@@ -725,26 +527,26 @@ const Library = [
         },
         {
             type: 'libraryFloorTable',
-            npc: table2,
-            isAccessible: true,
+            npc: null,
+            isAccessible: false,
             pathTo: null,
         },
         {
             type: 'libraryFloorTable',
-            npc: table3,
-            isAccessible: true,
+            npc: null,
+            isAccessible: false,
             pathTo: null,
         },
         {
             type: 'libraryFloorTable',
-            npc: table4,
-            isAccessible: true,
+            npc: null,
+            isAccessible: false,
             pathTo: null,
         },
         {
             type: 'libraryFloorTable',
-            npc: table5,
-            isAccessible: true,
+            npc: null,
+            isAccessible: false,
             pathTo: null,
         },
         {
@@ -755,8 +557,8 @@ const Library = [
         },
         {
             type: 'libraryFloorTable',
-            npc: table6,
-            isAccessible: true,
+            npc: null,
+            isAccessible: false,
             pathTo: null,
         },
         {
@@ -831,4 +633,4 @@ const Library = [
     ],
 ];
 
-export default Library;
+export default LibraryOpen;
--- src/NPC/librarian.js
+++ src/NPC/librarian.js
@@ -6,17 +6,25 @@ const Librarian = new NPC(
     '../assets/npc/librarian.png',
     [
         {
+            type: RequirementTypes.dialog,
+            message: `These thieves are stole Acadamia's most valuable book! Please, return the book to me!`,
+        },
+        {
+            type: RequirementTypes.dialog,
+            message: 'I think the thieves might have left some clues on the desks if you look around.',
+        },
+        {
             type: RequirementTypes.inventory,
             item: 'book',
-            amount: 10,
-            message: `These thieves are stealing Acadamia's most valuable books! Please, return all 10 books to me!`,
+            amount: 1,
+            message: `Please, return the book to me!`,
             afterAction: {
                 fn() {
                     this.addToInventory('report card', 1);
                 },
                 functionType: 'player',
                 message:
-                    'By the maths! I cannot thank you enough for retrieving these valuable books. Please, please take this Report Card! Oh, and could you protect those books until the Crime Subtracters arrest these thieves?',
+                    'By the maths! I cannot thank you enough for retrieving this valuable book. Please, please take this Report Card! Oh, and could you protect these books until the Crime Subtracters arrest these thieves?',
             },
         },
     ]
